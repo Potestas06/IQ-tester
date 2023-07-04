e3c5eeacc81e50d3df48ff36efdc7abb6f99a2af
Potestas06 <Patrick@krispel.com>
2023-07-04 15:50:25 +0200

    removed stuff

--- iq-ester/src/App.js
+++ iq-ester/src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import CatGPTChatbot from './components/Chat';
-import SaveUserInfo from './components/Share';
+import CatGPTChatbot from "./components/Chat";
 
 const App = () => {
   return (
     <div className="Container">
-      <h3 style={{ textAlign: 'right' }}>Share your IQ</h3>
-      <SaveUserInfo />
       <h1>Chat with CatGPT</h1>
       <CatGPTChatbot />
     </div>
